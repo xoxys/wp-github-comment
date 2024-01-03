9647a1ad2278aec2f3639eacda270db9861dab64
Robert Kaussow <mail@thegeeklab.de>
2024-01-03 22:33:03 +0100

    chore: cleanup docs

--- cmd/wp-github-comment/flags.go
+++ cmd/wp-github-comment/flags.go
@@ -6,12 +6,14 @@ import (
 )
 
 // settingsFlags has the cli.Flags for the plugin.Settings.
+//
+//go:generate go run docs.go flags.go
 func settingsFlags(settings *plugin.Settings, category string) []cli.Flag {
 	return []cli.Flag{
 		&cli.StringFlag{
 			Name:        "api-key",
 			EnvVars:     []string{"PLUGIN_API_KEY", "GITHUB_COMMENT_API_KEY"},
-			Usage:       "personal access token access github api",
+			Usage:       "personal access token to access the GitHub API",
 			Destination: &settings.APIKey,
 			Category:    category,
 			Required:    true,
@@ -19,7 +21,7 @@ func settingsFlags(settings *plugin.Settings, category string) []cli.Flag {
 		&cli.StringFlag{
 			Name:        "base-url",
 			EnvVars:     []string{"PLUGIN_BASE_URL", "GITHUB_COMMENT_BASE_URL"},
-			Usage:       "api url",
+			Usage:       "API URL",
 			Value:       "https://api.github.com/",
 			Destination: &settings.BaseURL,
 			Category:    category,
@@ -27,14 +29,14 @@ func settingsFlags(settings *plugin.Settings, category string) []cli.Flag {
 		&cli.StringFlag{
 			Name:        "key",
 			EnvVars:     []string{"PLUGIN_KEY", "GITHUB_COMMENT_KEY"},
-			Usage:       "unique identifier to assign to comment",
+			Usage:       "unique identifier to assign to a comment",
 			Destination: &settings.Key,
 			Category:    category,
 		},
 		&cli.StringFlag{
 			Name:        "message",
 			EnvVars:     []string{"PLUGIN_MESSAGE", "GITHUB_COMMENT_MESSAGE"},
-			Usage:       "file or string that holds the comment text",
+			Usage:       "path to file or string that contains the comment text",
 			Destination: &settings.Message,
 			Category:    category,
 			Required:    true,
@@ -42,7 +44,7 @@ func settingsFlags(settings *plugin.Settings, category string) []cli.Flag {
 		&cli.BoolFlag{
 			Name:        "update",
 			EnvVars:     []string{"PLUGIN_UPDATE", "GITHUB_COMMENT_UPDATE"},
-			Usage:       "update existing comment that matches the key",
+			Usage:       "enable update of an existing comment that matches the key",
 			Value:       false,
 			Destination: &settings.Update,
 			Category:    category,
@@ -50,7 +52,7 @@ func settingsFlags(settings *plugin.Settings, category string) []cli.Flag {
 		&cli.BoolFlag{
 			Name:        "skip-missing",
 			EnvVars:     []string{"PLUGIN_SKIP_MISSING", "GITHUB_COMMENT_SKIP_MISSING"},
-			Usage:       "skip creation of comment if message file does not exist",
+			Usage:       "skip comment creation if the given message file does not exist",
 			Value:       false,
 			Destination: &settings.SkipMissing,
 			Category:    category,
